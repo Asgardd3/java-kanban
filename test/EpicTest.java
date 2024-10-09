4531826fc25dfc76febc564d6df8141ff99df811
Alexandrov Anton <5845@bk.ru>
2024-10-10 01:49:50 +0300

    Добавляем абстрактный класс для тестов менеджера

--- test/EpicTest.java
+++ test/EpicTest.java
@@ -9,6 +9,9 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import managers.*;
 import tasks.*;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 
 class EpicTest {
     private TaskManager taskManager;
@@ -35,4 +38,59 @@ class EpicTest {
         epic1.addSubTasksId(epic1.getId());
         assertFalse(taskManager.getAllSubTasksByEpicId(epic1.getId()).equals(epic1.getId()));
     }
+
+    @Test
+    void shouldBeStatusNewThenAllSubtasksIsNew() throws ManagerSaveException {
+        Epic epic1 = new Epic("Эпик 1", "Описание 1");
+        SubTask subTask1 = new SubTask("Задача  1  (jUnit)", "Описание  1", Status.NEW, 1, LocalDateTime.now(), Duration.ofMinutes(30));
+        SubTask subTask2 = new SubTask("Задача  2 (jUnit)", "Описание  1", Status.NEW, 1, LocalDateTime.now().plusHours(1), Duration.ofMinutes(30));
+        this.taskManager.addEpic(epic1);
+        epic1.addSubTasksId(subTask1.getId());
+        epic1.addSubTasksId(subTask2.getId());
+
+        assertEquals(epic1.getStatus(), Status.NEW);
+
+    }
+
+    @Test
+    void shouldBeStatusDoneThenAllSubtasksIsDone() throws ManagerSaveException {
+        Epic epic1 = new Epic("Эпик 1", "Описание 1");
+        SubTask subTask1 = new SubTask("Задача  1  (jUnit)", "Описание  1", Status.NEW, 1, LocalDateTime.now(), Duration.ofMinutes(30));
+        SubTask subTask2 = new SubTask("Задача  2 (jUnit)", "Описание  1", Status.NEW, 1, LocalDateTime.now().plusHours(1), Duration.ofMinutes(30));
+        this.taskManager.addEpic(epic1);
+        subTask1.setStatus(Status.DONE);
+        subTask2.setStatus(Status.DONE);
+
+        epic1.addSubTasksId(subTask1.getId());
+        epic1.addSubTasksId(subTask2.getId());
+        assertEquals(epic1.getStatus(), Status.NEW);
+    }
+
+    @Test
+    void shouldBeStatusNewThenAllSubtasksIsDoneAndNew() throws ManagerSaveException {
+        Epic epic1 = new Epic("Эпик 1", "Описание 1");
+        SubTask subTask1 = new SubTask("Задача  1  (jUnit)", "Описание  1", Status.NEW, 1, LocalDateTime.now(), Duration.ofMinutes(30));
+        SubTask subTask2 = new SubTask("Задача  2 (jUnit)", "Описание  1", Status.NEW, 1, LocalDateTime.now().plusHours(1), Duration.ofMinutes(30));
+        this.taskManager.addEpic(epic1);
+        subTask1.setStatus(Status.DONE);
+
+        epic1.addSubTasksId(subTask1.getId());
+        epic1.addSubTasksId(subTask2.getId());
+        assertEquals(epic1.getStatus(), Status.NEW);
+    }
+
+    @Test
+    void shouldBeStatusInProgressThenAllSubtasksIsInProgress() throws ManagerSaveException {
+        Epic epic1 = new Epic("Эпик 1", "Описание 1");
+        SubTask subTask1 = new SubTask("Задача  1  (jUnit)", "Описание  1", Status.NEW, 1, LocalDateTime.now(), Duration.ofMinutes(30));
+        SubTask subTask2 = new SubTask("Задача  2 (jUnit)", "Описание  1", Status.NEW, 1, LocalDateTime.now().plusHours(1), Duration.ofMinutes(30));
+        this.taskManager.addEpic(epic1);
+        subTask1.setStatus(Status.IN_PROGRESS);
+        subTask2.setStatus(Status.IN_PROGRESS);
+
+        epic1.addSubTasksId(subTask1.getId());
+        epic1.addSubTasksId(subTask2.getId());
+        assertEquals(epic1.getStatus(), Status.NEW);
+    }
+
 }
